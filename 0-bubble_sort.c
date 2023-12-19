a3ab1660ac5d243351af66449617c00a162a00f3
Librazionpc <nwajeizion393@gmail.com>
2023-12-19 23:51:37 +0100

    checker2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -31,7 +31,7 @@ void bubble_sort(int *array, size_t size)
 	for (j = 0; j < size - 1; j++)
 	{
 		check = 0;
-		for (i = 0; i < size - 1; i++)
+		for (i = 0; i < size - j - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
